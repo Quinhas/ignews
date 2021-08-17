5862640b44d73bee77375ef68f0258cb5b65acbc
Quinhas <ln.santana08@gmail.com>
2021-08-17 00:24:41 -0300

    Estrutura da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default Home;
