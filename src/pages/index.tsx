39cb5dcf77d143a2f012ea957dc2024bae7d708d
Quinhas <ln.santana08@gmail.com>
2021-08-17 20:04:59 -0300

    Componentes e páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +9,22 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 };
