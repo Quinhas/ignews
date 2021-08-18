0f12c996c2e445243c648f495eb15dabcb8baa9d
Quinhas <ln.santana08@gmail.com>
2021-08-17 22:29:03 -0300

    Integração com API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-const Home: NextPage = () => {
+type HomeProps = {
+  product: { priceId: string; amount: number };
+};
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,15 +23,35 @@ const Home: NextPage = () => {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>
+              for{" "}
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(product.amount)}{" "}
+              month
+            </span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
-};
+}
 
-export default Home;
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JPcPsKZKv6QZmBxg5bWf7tR");
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount ?? 0) / 100,
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
---
+++ src/services/stripe.ts
@@ -0,0 +1,6 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(String(process.env.STRIPE_API_KEY), {
+  apiVersion: "2020-08-27",
+  appInfo: { name: "ig.news", version: "0.1.0" },
+});
