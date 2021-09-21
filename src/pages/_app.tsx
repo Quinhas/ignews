a7fc58697d59e59fdc1f0a564c4f8bd650152737
Quinhas <ln.santana08@gmail.com>
2021-09-21 08:20:25 -0300

    Autenticação com Next Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider as NextAuthProvider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Header } from "../components/Header";
@@ -5,13 +6,13 @@ import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Head>
         <title>ig.news</title>
       </Head>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 export default MyApp;
